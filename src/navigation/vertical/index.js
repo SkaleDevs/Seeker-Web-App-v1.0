31d1a05d1a5e21360856375bfa3f2cfd046efc15
FuryACE <sudhanshuvshekhar@gmail.com>
2022-07-18 00:12:18 +0530

    removed uselss components

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -1,26 +1,29 @@
 // ** Icon imports
-import Login from 'mdi-material-ui/Login'
-import Table from 'mdi-material-ui/Table'
-import CubeOutline from 'mdi-material-ui/CubeOutline'
-import HomeOutline from 'mdi-material-ui/HomeOutline'
-import FormatLetterCase from 'mdi-material-ui/FormatLetterCase'
-import AccountCogOutline from 'mdi-material-ui/AccountCogOutline'
-import CreditCardOutline from 'mdi-material-ui/CreditCardOutline'
-import AccountPlusOutline from 'mdi-material-ui/AccountPlusOutline'
-import AlertCircleOutline from 'mdi-material-ui/AlertCircleOutline'
-import GoogleCirclesExtended from 'mdi-material-ui/GoogleCirclesExtended'
+import Login from "mdi-material-ui/Login";
+import Table from "mdi-material-ui/Table";
+import CubeOutline from "mdi-material-ui/CubeOutline";
+import HomeOutline from "mdi-material-ui/HomeOutline";
+import FormatLetterCase from "mdi-material-ui/FormatLetterCase";
+import AccountCogOutline from "mdi-material-ui/AccountCogOutline";
+import CreditCardOutline from "mdi-material-ui/CreditCardOutline";
+import AccountPlusOutline from "mdi-material-ui/AccountPlusOutline";
+import AlertCircleOutline from "mdi-material-ui/AlertCircleOutline";
+import GoogleCirclesExtended from "mdi-material-ui/GoogleCirclesExtended";
 
 const navigation = () => {
   return [
     {
-      title: 'Dashboard',
-      icon: HomeOutline,
-      path: '/'
+      sectionTitle: "Home",
     },
     {
-      title: 'Account Settings',
+      title: "Dashboard",
+      icon: HomeOutline,
+      path: "/",
+    },
+    {
+      title: "Account Settings",
       icon: AccountCogOutline,
-      path: '/account-settings'
+      path: "/account-settings",
     },
     // {
     //   sectionTitle: 'Pages'
@@ -71,7 +74,7 @@ const navigation = () => {
     //   title: 'Form Layouts',
     //   path: '/form-layouts'
     // }
-  ]
-}
+  ];
+};
 
-export default navigation
+export default navigation;
---
+++ src/pages/funding_agency/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home_agency = () => {
+  return (
+    <div>Home_agency</div>
+  )
+}
+
+export default Home_agency

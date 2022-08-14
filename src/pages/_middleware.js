5a6f51fe2575d3b391d2e55905ff2af73f3ac51c
kumar <harshme78@gmail.com>
2022-08-14 23:21:26 +0530

    nothing

--- src/pages/_middleware.js
+++ src/pages/_middleware.js
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
-// import { unstable_getServerSession } from "next-auth/next"
+// import {authOptions} from './api/auth/[...nextauth]'
 import {getSession} from 'next-auth/react';
+// import { unstable_getServerSession } from "next-auth/next"
+
 
 
 const isModeratorRoute = (pathname) => {
@@ -18,15 +20,19 @@ const isAgencyRoute = (pathname) => {
 
 export async function middleware(req,res) {
     console.log("middleware");
-    console.log(req);
-    const session = await getSession(req);
-    console.log("hele");
-    if(!session){
-        return NextResponse.redirect(new URL('/401', req.url));
-   }
-    const role = session?.user?.role;
-
-  // const { pathname } = req.nextUrl;
+    // console.log(req);
+    const session = await getSession({req});
+    // console.log(authOptions);
+    // const session1 = await unstable_getServerSession(req,res,authOptions);
+    console.log(session);
+    // console.log(session1);
+    //   if(!session){
+      //       return NextResponse.redirect(new URL('/401', req.url));
+      //  }
+      const role = session?.user?.role;
+      
+      const { pathname } = req.nextUrl;
+      console.log(pathname);
  
 
   // if (isModeratorRoute(pathname) && role !== "moderator") {

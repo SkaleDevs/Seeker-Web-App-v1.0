63a6163370f733a3f14f0d249bcb8dc2cddb817a
FuryACE <sudhanshuvshekhar@gmail.com>
2022-08-17 02:43:59 +0530

    added pwa features

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
-const path = require('path')
+const withPWA = require("next-pwa");
 
-module.exports = {
-  trailingSlash: true,
-  reactStrictMode: false,
-  experimental: {
-    esmExternals: false,
-    jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
   },
-  webpack: config => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
-    }
-
-    return config
-  }
-}
+});

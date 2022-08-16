78c7756f7898db75eed900e7b6fa95486a885bd1
FuryACE <sudhanshuvshekhar@gmail.com>
2022-08-17 03:17:01 +0530

    fixes

--- next.config.js
+++ next.config.js
@@ -1,3 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  trailingSlash: true,
+  reactStrictMode: false,
+  experimental: {
+    esmExternals: false,
+    jsconfigPaths: true, // enables it for both jsconfig.json and tsconfig.json
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      apexcharts: path.resolve(
+        __dirname,
+        "./node_modules/apexcharts-clevision"
+      ),
+    };
+
+    return config;
+  },
+};
+
 const withPWA = require("next-pwa");
 
 module.exports = withPWA({

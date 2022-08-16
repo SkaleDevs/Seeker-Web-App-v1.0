1ad367e78bca21cb301b0ada2e5124c22e879fce
Shreya Narayan <shreyanushka02@gmail.com>
2022-08-16 13:11:20 +0530

    done

--- src/pages/api/controller/agency/createScholarship.js
+++ src/pages/api/controller/agency/createScholarship.js
@@ -1,4 +1,4 @@
-import Scheme from '../../models/Scheme';
+import Scheme from '../../model/agencySchemeSchema';
 import connectDB from '../../auth/lib/connectDB';
 import {getSession} from 'next-auth/react';
 connectDB();
@@ -7,9 +7,20 @@ export default async function handler(req,res){
     if (!session || session.user.role!=="agency") {
     return res.status(401).json({error: 'Unauthorized'})
     }
-    let data=  await Agency.findOneAndUpdate({email:req.body.email},req.body);
+    let data =  await Scheme.findOneAndUpdate({name:req.body.name},req.body);
     if(data){
         return res.send(data);
     }
-    return res.send("No profile exists")
+    const details=new Scheme({
+        name:req.body.name,
+         schemeType:req.body.schemeType,
+         agencyDescription:req.body.agencyDescription,
+         eligibility:req.body.eligibility,
+        deadline:req.body.deadline,
+        maxAmount:req.body.maxAmount,
+        extraDetailFile:req.body.extraDetailFile,
+        noOfApplications:0,
+    })
+    details.save()
+    res.send(details)
 }

51417c560b60fb7a86c3a01bb7ffa86d6d624fac
FuryACE <sudhanshuvshekhar@gmail.com>
2022-08-17 14:41:26 +0530

    pwa deploy issue resolve

--- next.config.js
+++ next.config.js
@@ -21,11 +21,15 @@ module.exports = {
 };
 
 const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  },
+	reactStrictMode: true,
+	pwa: {
+		dest: "public",
+		register: true,
+		skipWaiting: true,
+		runtimeCaching,
+		buildExcludes: [/middleware-manifest.json$/]
+	}
 });
